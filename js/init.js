d1c68bdbca6c0b96bd2e142c5cc8482c6bc047f7
Mike_from_git_native <apps.for.zero@gmail.com>
2016-11-11 03:41:45 +0200

    в Index.html добавлен класс  jump при щелчке по которому проиходит анимация с плавностью easeOutExpo (http://easings.net/ru) и длительностью 2000 изменить все можно в init.js

--- js/init.js
+++ js/init.js
@@ -3,12 +3,13 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-    $(document).ready(function(){
-        $('.scrollspy').scrollSpy();
-      });
 
-
-
-
+    $(".jump").on("click", function (event)
+     {
+        event.preventDefault();
+        var i  = $(this).attr('href');
+        t = $(i).offset().top;
+        $('body,html').animate({scrollTop: t}, 2000, 'easeOutExpo');
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
