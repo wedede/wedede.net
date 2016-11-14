b14fffdac8541f4728f29421d105fbb9e548e32a
Mike_from_git_native <apps.for.zero@gmail.com>
2016-11-14 21:18:51 +0200

    geo redirect

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+require_once('geoip.php');
+
+$geoplugin = new geoPlugin();
+$geoplugin->locate();
+
+// create a variable for the country code
+$var_country_code = $geoplugin->countryCode;
+
+$ru_array = array("AM", "AZ", "BY", "GE", "KZ","MD","RU","TJ","TM","UA");
+//Армения,Азербайджан,Беларусь,Грузия,Казахстан, Молдова, Россия,Таджикистан,Туркменистан, Украина
+
+// redirect based on country code:
+if (in_array($var_country_code, $ru_array)) {
+header('Location:index-ru.html');
+}
+else {
+header('Location:index-eng.html');
+}
+?>
