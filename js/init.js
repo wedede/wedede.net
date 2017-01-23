2ddf2e6da66394bf362a63b9144708c23afc1599
Mike_from_git_native <apps.for.zero@gmail.com>
2017-01-23 21:28:36 +0200

    backend is ready, public Slack channel link added

--- js/init.js
+++ js/init.js
@@ -29,7 +29,50 @@
       
     $('#send_callback').click(function()
       {
-          console.log('ok');
+        console.log('ok');
+        if($('#callback_email').hasClass('valid') || $('#callback_phone').hasClass('valid')) 
+        {
+            //create object
+            var data={}
+            
+            data['callback_email']=$('#callback_email').val();
+            data['callback_phone']=$('#callback_phone').val();
+            
+            console.log(data);
+            
+            Materialize.toast('Отправка данных...',60000);
+
+          $.post( "/callback.php", { jsonData: JSON.stringify(data)})
+		  .done(function( returned_data ) 
+		  {	
+			$('.toast').remove(); 
+            $('#callback').modal('close');
+              
+             console.log(returned_data);
+
+			if(returned_data.result=='sended') 
+			{	
+                Materialize.toast('Спасибо за контакт, мы свяжемся с вами в ближайшее время', 6000);
+			}
+            if(returned_data.result=='limit') 
+			{	
+                Materialize.toast('Благоарим вас, ваши данные были отосланы ранее', 4000);
+			}
+              if(returned_data.result=='empty')
+			{
+             	Materialize.toast('Что то определенно пошло не так... и мы уже ковыряемся', 4000);
+            }
+		  })
+		.fail(function() 
+		{
+			Materialize.toast('Ошибка отправки', 4000);
+            console.log(returned_data);
+		})
+      }
+        else 
+        {
+            Materialize.toast('Проверьте пожалуйста введенные данные', 4000);
+        }
        
       });
   }); // end of document ready
